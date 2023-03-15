d55d14aa1f6633c2234d3ced4c273a2f7c128a4d
congtranc0722g1 <trantancongall@gmail.com>
2023-03-15 20:49:34 +0700

    Nop

--- mobile_store.sql
+++ mobile_store.sql
@@ -18,32 +18,13 @@ email varchar(255)
  foreign key(id_user) references `user`(id),
   foreign key(id_role) references `role`(id)
  );
-create table commodity(
-id int auto_increment primary key,
-`name` varchar(255),
-`cpu` varchar(255),
-capacity varchar(255),
-trademark varchar(255),
-price double,
-image varchar(255),
-camera varchar(255),
-selfie varchar(255),
-screensize varchar(255),
-guarantee varchar(255),
-origin varchar(255),
-`description` varchar(255),
-code_qr varchar(255) ,
-quantity int,
-id_supplier int,
-foreign key(id_supplier) references supplier(id)	
-);
 create table supplier(
 id int auto_increment primary key,
-code varchar(6) unique,
-`name` varchar(255),
-address varchar(255),
-phone_number varchar(255),
-email varchar(255)
+`code` varchar(6) not null unique,
+`name` varchar(255) not null,
+address varchar(255) not null,
+phone_number varchar(255) not null unique,
+email varchar(255) not null unique
 );
 create table customer(
 id int auto_increment primary key,
@@ -61,6 +42,12 @@ foreign key(id_user) references user(id)
 -- id_customer int,
 -- quantity int
 -- );
+create table bill (
+id int auto_increment primary key,
+id_customer int,
+payment_method varchar(255),
+foreign key(id_customer) references customer(id)
+);
 create table bill_history (
 id int auto_increment primary key,
 buy_date date,
@@ -68,18 +55,6 @@ id_bill int,
 detail varchar(255),
 money_total double,
 foreign key(id_bill) references bill(id)
-);
-create table bill (
-id int auto_increment primary key,
-id_customer int,
-payment_method varchar(255),
-foreign key(id_customer) references customer(id)
-);
-create table imports_commodity_history (
-id int auto_increment primary key,
-date_imports date,
-id_commodity int,
-foreign key(id_commodity) references commodity(id)
 );
 create table employee(
 id int auto_increment primary key,
@@ -94,3 +69,30 @@ foreign key(id_user) references user(id)
 );
 -- create table bill_detail(
 -- );
+create table commodity(
+id int auto_increment primary key,
+`name` varchar(255),
+`cpu` varchar(255),
+capacity varchar(255),
+trademark varchar(255),
+price double,
+image varchar(255),
+camera varchar(255),
+selfie varchar(255),
+screensize varchar(255),
+guarantee varchar(255),
+origin varchar(255),
+`description` varchar(255),
+code_qr varchar(255) ,
+quantity int,
+id_bill int,
+foreign key(id_bill) references bill(id)
+);
+create table imports_commodity_history (
+id int auto_increment primary key,
+date_imports date,
+id_commodity int,
+id_supplier int,
+foreign key(id_commodity) references commodity(id),
+foreign key(id_supplier) references supplier(id)
+);
